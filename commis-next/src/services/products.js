e86d96adbb9e58d4f25a070abe0bd02478c297c2
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2025-05-11 16:10:42 +0300

    add

--- commis-next/src/services/products.js
+++ commis-next/src/services/products.js
@@ -1,17 +1,70 @@
+// import api from './api';
+
+// export const getProducts = async () => {
+//     const response = await api.get('/products/all');
+//     return response.data;
+// };
+
+// export const getProductById = async (productId) => {
+//     const response = await api.get(`/products/${productId}`);
+//     return response.data;
+// };
+
+// export const getUserProducts = async (accessToken) => {
+//     const response = await api.get('/products', {
+//         headers: {
+//             Authorization: `Bearer ${accessToken}`
+//         }
+//     });
+//     return response.data;
+// };
+
+// export const searchProducts = async (query) => {
+//     const response = await api.get(`/products/search?query=${query}`);
+//     return response.data;
+// };
+
+// export const addProduct = async (formData) => {
+//     const response = await api.post('/products', formData, {
+//         headers: {
+//             'Content-Type': 'multipart/form-data'
+//         }
+//     });
+//     return response.data;
+// };
+
+// export const updateProduct = async (productId, productData) => {
+//     const response = await api.patch(`/products/${productId}`, productData);
+//     return response.data;
+// };
+
+// export const deleteProduct = async (productId) => {
+//     const response = await api.delete(`/products/${productId}`);
+//     return response.data;
+// };
+
+// export const deleteImage = async (productId, indices) => {
+//     const response = await api.delete(`/products/${productId}/images`, {
+//         data: { indices }
+//     });
+//     return response.data;
+// };
+
+
 import api from './api';
 
 export const getProducts = async () => {
-    const response = await api.get('/products/all');
+    const response = await api.get(`${process.env.NEXT_PUBLIC_API_URL}/products/all`);
     return response.data;
 };
 
 export const getProductById = async (productId) => {
-    const response = await api.get(`/products/${productId}`);
+    const response = await api.get(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`);
     return response.data;
 };
 
 export const getUserProducts = async (accessToken) => {
-    const response = await api.get('/products', {
+    const response = await api.get(`${process.env.NEXT_PUBLIC_API_URL}/products`, {
         headers: {
             Authorization: `Bearer ${accessToken}`
         }
@@ -20,12 +73,12 @@ export const getUserProducts = async (accessToken) => {
 };
 
 export const searchProducts = async (query) => {
-    const response = await api.get(`/products/search?query=${query}`);
+    const response = await api.get(`${process.env.NEXT_PUBLIC_API_URL}/products/search?query=${query}`);
     return response.data;
 };
 
 export const addProduct = async (formData) => {
-    const response = await api.post('/products', formData, {
+    const response = await api.post(`${process.env.NEXT_PUBLIC_API_URL}/products`, formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
@@ -34,17 +87,17 @@ export const addProduct = async (formData) => {
 };
 
 export const updateProduct = async (productId, productData) => {
-    const response = await api.patch(`/products/${productId}`, productData);
+    const response = await api.patch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`, productData);
     return response.data;
 };
 
 export const deleteProduct = async (productId) => {
-    const response = await api.delete(`/products/${productId}`);
+    const response = await api.delete(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`);
     return response.data;
 };
 
 export const deleteImage = async (productId, indices) => {
-    const response = await api.delete(`/products/${productId}/images`, {
+    const response = await api.delete(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/images`, {
         data: { indices }
     });
     return response.data;
