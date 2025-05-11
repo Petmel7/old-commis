63840a0958bd8772660f41dccf89d97ca281d7f2
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2025-05-11 12:31:03 +0300

    add

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,17 +5,6 @@ const path = require('path');
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     dialect: 'postgres',
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000,
-//     },
-// });
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
@@ -42,3 +31,17 @@ sequelize.authenticate()
     });
 
 module.exports = sequelize;
+
+
+
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// });
