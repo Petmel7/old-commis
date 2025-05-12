8f9a224cc7656d243ba0f6f658c719e6af9861e8
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2025-05-12 11:14:12 +0300

    add

--- commis-next/src/services/api.js
+++ commis-next/src/services/api.js
@@ -100,10 +100,10 @@
 
 import axios from 'axios';
 
-const isLocalhost = typeof window !== 'undefined' && window.location.hostname === 'localhost';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const api = axios.create({
-    baseURL: isLocalhost
+    baseURL: !isProduction
         ? 'http://localhost:5000/api'
         : `${process.env.NEXT_PUBLIC_API_URL}/api`,
     headers: {
