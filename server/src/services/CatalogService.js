aec1ae3102e20fed9a6692908f09ba3d379faf1e
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2025-05-22 19:06:10 +0300

    add

--- server/src/services/CatalogService.js
+++ server/src/services/CatalogService.js
@@ -1,13 +1,32 @@
 const { Product, Subcategory } = require('../models');
 const { validateProduct } = require('../validators/validators');
+const { Op } = require('sequelize');
+
+// const getProductsByCategory = async (category) => {
+
+//     const subcategoryRecord = await Subcategory.findOne({ where: { name: category } });
+
+//     if (!subcategoryRecord) {
+//         return [];
+//     }
+
+//     const products = await Product.findAll({
+//         where: { subcategory_id: subcategoryRecord.id },
+//     });
+
+//     return products.map(validateProduct);
+// };
 
 const getProductsByCategory = async (category) => {
+    const subcategoryRecord = await Subcategory.findOne({
+        where: {
+            name: {
+                [Op.iLike]: category // нечутливий до регістру пошук
+            }
+        }
+    });
 
-    const subcategoryRecord = await Subcategory.findOne({ where: { name: category } });
-
-    if (!subcategoryRecord) {
-        return [];
-    }
+    if (!subcategoryRecord) return [];
 
     const products = await Product.findAll({
         where: { subcategory_id: subcategoryRecord.id },
