f4f723b4eb8d85d2f6fb3f193ebb9704d02897ef
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2025-05-23 19:44:06 +0300

    add

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -3,6 +3,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 // const { getGoogleCallbackUrl } = require('../utils/env');
+const { getServerUrl } = require('../utils/env');
 
 // console.log('✅✅getGoogleCallbackUrl', getGoogleCallbackUrl());
 
@@ -10,7 +11,8 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // callbackURL: getGoogleCallbackUrl()
-    callbackURL: '/api/users/google/callback'
+    // callbackURL: '/api/users/google/callback'
+    callbackURL: `${getServerUrl()}/api/users/google/callback`
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
