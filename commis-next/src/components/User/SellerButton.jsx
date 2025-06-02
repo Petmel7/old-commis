81ad5de1a55bf2ad6e2d1519d94ad9daaf07c5b1
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2025-06-02 15:15:53 +0300

    add

--- commis-next/src/components/User/SellerButton.jsx
+++ commis-next/src/components/User/SellerButton.jsx
@@ -21,35 +21,3 @@ const SellerButton = () => {
 };
 
 export default SellerButton;
-
-
-
-
-// import { useRouter } from 'next/router';
-// import { useAuth } from '@/context/AuthContext';
-// import styles from './styles/Auth.module.css';
-
-// const SellerButton = () => {
-//     const { isRegistered, isGoogleRegistered, user } = useAuth();
-//     const router = useRouter();
-
-//     const handleClick = () => {
-//         const registered = isRegistered || isGoogleRegistered;
-//         if (!registered) {
-//             router.push('/register');
-//         } else {
-//             router.push('/productAdd');
-//         }
-//     };
-
-//     if (!user || user.role !== 'buyer') return null;
-
-//     return (
-//         <div className={styles.authButtonConteaner}>
-//             <span className={styles.authText}>Хочете стати продавцем?</span>
-//             <button className={styles.authButton} onClick={handleClick}>Так</button>
-//         </div>
-//     );
-// };
-
-// export default SellerButton;
