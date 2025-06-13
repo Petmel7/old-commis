bed2cd23966e0dfd207f7c9b48f367d6f7932413
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2025-06-13 10:10:45 +0300

    add

--- commis-next/src/hooks/useLoadingAndError.js
+++ commis-next/src/hooks/useLoadingAndError.js
@@ -1,8 +1,39 @@
-import React from 'react';
+// import React from 'react';
+// import Loading from '../components/Loading/Loading';
+// import ErrorDisplay from '../components/ErrorDisplay/ErrorDisplay';
+
+// const useLoadingAndError = (loading, error) => {
+//     if (loading) return <Loading />;
+//     if (error) return <ErrorDisplay error={error} />;
+//     return null;
+// };
+
+// export default useLoadingAndError;
+
+import React, { useEffect, useState } from 'react';
 import Loading from '../components/Loading/Loading';
 import ErrorDisplay from '../components/ErrorDisplay/ErrorDisplay';
 
 const useLoadingAndError = (loading, error) => {
+    const [showDelayAlert, setShowDelayAlert] = useState(false);
+
+    useEffect(() => {
+        let timer;
+        if (loading) {
+            timer = setTimeout(() => {
+                setShowDelayAlert(true);
+            }, 5000);
+        } else {
+            clearTimeout(timer);
+            setShowDelayAlert(false);
+        }
+        return () => clearTimeout(timer);
+    }, [loading]);
+
+    if (showDelayAlert) {
+        alert('У зв\u044fзку з безкоштовним хостингом перше завантаження може тривати довше. Зачекайте, будь ласка.');
+    }
+
     if (loading) return <Loading />;
     if (error) return <ErrorDisplay error={error} />;
     return null;
